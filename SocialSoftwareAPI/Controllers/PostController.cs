888631b84733daf3687356bd4017e852116e2515
June0822 <junehowtodo@gmail.com>
2022-08-06 23:55:21 +0800

    Build PostController and 'Post' Table in mssql

---
+++ SocialSoftwareAPI/Controllers/PostController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using SocialSoftwareAPI.Models;
+
+namespace SocialSoftwareAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PostController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select PostId, Title, Content from dbo.Posts";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SocialSoftwareAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Post post)
+        {
+            string query = @"insert into dbo.Posts values(
+                '" + post.Title + @"'
+                ,'" + post.Content + @"'
+                )";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SocialSoftwareAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Post post)
+        {
+            string query = @"update dbo.Posts set
+                Title = '" + post.Title + @"'
+                ,Content = '" + post.Content + @"'
+                where PostId = '" + post.PostId + @"'";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SocialSoftwareAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Update Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from dbo.Posts
+                where PostId = '" + id + @"'";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SocialSoftwareAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Delete Successfully");
+        }
+    }
+}
---
+++ SocialSoftwareAPI/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialSoftwareAPI.Models
+{
+    public sealed class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(length:100000)]
+
+        public string Content { get; set; } = string.Empty;
+
+    }
+}
